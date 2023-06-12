bd7b18ec1a4e0dfc2fa602275c44629c9193075b
Stephane Koeniguer <stephane.koeniguer@gmail.com>
2023-06-12 16:10:02 +0200

    update shop

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Symfony\UX\Autocomplete\AutocompleteBundle::class => ['all' => true],
+    Symfony\UX\Swup\SwupBundle::class => ['all' => true],
 ];
