08ed51eb59aec1f8fe6bbecc21c702cfb9420e9f
Stephane Koeniguer <stephane.koeniguer@gmail.com>
2023-06-24 21:10:53 +0200

    update contact mail

--- src/Controller/Form/ContactForm.php
+++ src/Controller/Form/ContactForm.php
@@ -7,45 +7,45 @@ use Symfony\Component\Validator\Constraints as Assert;
 class ContactForm
 {
     #[Assert\NotBlank(message: "Le champ Nom est requis")]
-    private $name;
+    private ?string $name;
 
     #[Assert\NotBlank(message: "Le champ e-mail est requis")]
     #[Assert\Email(message: "L'adresse email n'est pas valide")]
-    private $email;
+    private ?string $email;
 
     #[Assert\NotBlank(message: 'Le champ message est requis')]
-    private $content;
+    private ?string $content;
 
-    public function getName()
+    public function getName(): ?string
     {
         return $this->name;
     }
 
-    public function setName($name)
+    public function setName(string $name): self
     {
         $this->name = $name;
 
         return $this;
     }
 
-    public function getEmail()
+    public function getEmail(): ?string
     {
         return $this->email;
     }
 
-    public function setEmail($email)
+    public function setEmail(string $email): self
     {
         $this->email = $email;
 
         return $this;
     }
 
-    public function getContent()
+    public function getContent(): ?string
     {
         return $this->content;
     }
 
-    public function setContent($content)
+    public function setContent(string $content): self
     {
         $this->content = $content;
 
