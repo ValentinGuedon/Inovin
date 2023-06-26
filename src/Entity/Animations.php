ec64024956ed5d3918271427c882b8b541628e82
Ludivine Martel <mrt.ludivine@gmail.com>
2023-06-26 17:33:18 +0200

    formulaire réservation atelier et panier atelier

--- src/Entity/Animations.php
+++ src/Entity/Animations.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\AnimationsRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: AnimationsRepository::class)]
@@ -14,10 +16,10 @@ class Animations
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
-    private ?string $name = null;
+    private ?string $nom = null;
 
     #[ORM\Column]
-    private ?int $price = null;
+    private ?int $prix = null;
 
     #[ORM\Column(length: 2000)]
     private ?string $resume = null;
@@ -28,31 +30,40 @@ class Animations
     #[ORM\Column(length: 255)]
     private ?string $image = null;
 
+    #[ORM\OneToMany(mappedBy: 'animations', targetEntity: Atelier::class)]
+    private Collection $ateliers;
+
+    public function __construct()
+    {
+        $this->ateliers = new ArrayCollection();
+    }
+
+
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getName(): ?string
+    public function getNom(): ?string
     {
-        return $this->name;
+        return $this->nom;
     }
 
-    public function setName(string $name): self
+    public function setNom(string $nom): self
     {
-        $this->name = $name;
+        $this->nom = $nom;
 
         return $this;
     }
 
-    public function getPrice(): ?int
+    public function getPrix(): ?int
     {
-        return $this->price;
+        return $this->prix;
     }
 
-    public function setPrice(int $price): self
+    public function setPrix(int $prix): self
     {
-        $this->price = $price;
+        $this->prix = $prix;
 
         return $this;
     }
@@ -89,6 +100,36 @@ class Animations
     public function setImage(string $image): self
     {
         $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Atelier>
+     */
+    public function getAteliers(): Collection
+    {
+        return $this->ateliers;
+    }
+
+    public function addAtelier(Atelier $atelier): static
+    {
+        if (!$this->ateliers->contains($atelier)) {
+            $this->ateliers->add($atelier);
+            $atelier->setAnimations($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAtelier(Atelier $atelier): static
+    {
+        if ($this->ateliers->removeElement($atelier)) {
+            // set the owning side to null (unless already changed)
+            if ($atelier->getAnimations() === $this) {
+                $atelier->setAnimations(null);
+            }
+        }
 
         return $this;
     }
