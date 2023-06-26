cdbc5c9ae552570e22638af9bc0b6ae0c20039bb
Stephane Koeniguer <103316525+StephaneKoeniguer@users.noreply.github.com>
2023-06-26 10:11:50 +0200

    Update CartShopService.php

--- src/Service/CartShopService.php
+++ src/Service/CartShopService.php
@@ -28,7 +28,6 @@ class CartShopService
         }
 
         // Mise a jour du panier
-      
         $session->getFlashBag()->add('sk-alert', 'Votre panier a été mis à jour'); /* @phpstan-ignore-line */
 
         $panier = $session->set('panier', $panier);
