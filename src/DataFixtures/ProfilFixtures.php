4309803490dd9feb7f63649786aca5648645190b
ValentinGuedon <guedonvalentin@gmail.com>
2023-06-30 14:13:11 +0200

    fixed bugs

--- src/DataFixtures/ProfilFixtures.php
+++ src/DataFixtures/ProfilFixtures.php
@@ -11,10 +11,10 @@ use Symfony\Component\String\Slugger\SluggerInterface;
 class ProfilFixtures extends Fixture
 {
     private const PROFILS = [
-        ['Le chardo-né','Vous aimez tout ce qui est sec, acide et frais. Vous êtes parisien.ne ?','chardo-ne.png'],
+        ['Le chardo-né','Vous aimez tout ce qui est sec, acide et frais. Vous êtes parisien.ne ?'],
         ['Le terrien','Vous aimez les vins râpeux et puissant, de ceux qui sillonnent votre palais
-        à grands coups de tanins','terrien.png'],
-        ['Le bonbon','Pas de doute, paradis sucré est la plus juste description de votre cave idéale','bonbon.png']
+        à grands coups de tanins'],
+        ['Le bonbon','Pas de doute, paradis sucré est la plus juste description de votre cave idéale']
     ];
 
     private SluggerInterface $slugger;
@@ -32,10 +32,6 @@ class ProfilFixtures extends Fixture
             $profil->setDescription($profilData[1]);
             $slug = $this->slugger->slug($profil->getName());
             $profil->setSlug($slug);
-            $imageFilename = $profilData[2];
-            $imagePath = __DIR__ . '\..\..\public\assets\images\profils\\' . $imageFilename;
-            $uploadedImage = new UploadedFile($imagePath, $imageFilename);
-            $profil->setProfilFile($uploadedImage);
             $manager->persist($profil);
             $this->addReference($profilData[0], $profil);
         }
